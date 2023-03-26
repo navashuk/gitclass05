3221731d911d2a7bcf1111827e5f9b8380d77de2
navashuk <mr.shukroew@gmail.com>
2023-03-26 13:19:22 -0400

    my first commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello city");
-        System.out.println("hello citddy");
 
-
-
-
+        for (int i =0; i <10 ; i++) {
+            System.out.println(i);
+        }
     }
-
 }
